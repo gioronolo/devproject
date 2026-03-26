06ca0eb1565ca2f46648358cdf0b954040084885
gioronolo <gio@virtualinnovation.co.nz>
2026-03-26 22:18:22 +0800

    Update ghl-connector.php

--- wp-content/plugins/ghl-connector/ghl-connector.php
+++ wp-content/plugins/ghl-connector/ghl-connector.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: GHL Connector - GIO
+ * Plugin Name: GHL Connector
  * Description: Connect WordPress to GoHighLevel - Contacts & Form Embed Manager.
  * Version: 3.0.0
  * Author: Your Name
